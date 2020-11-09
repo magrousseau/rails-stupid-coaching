4fd553a4100aa4a92bd8bbc6caf105684eba622f
Magali Rousseau <rousseaumagali@gmail.com>
2020-11-09 14:34:43 +0100

    exercise completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'ask', to:'_questions#ask'
+  get 'answer', to:'_questions#answer'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/system/questions_test.rb
@@ -0,0 +1,20 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  class QuestionsTest < ApplicationSystemTestCase
+  # [...]
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "question", with: "Hello"
+    click_on "Ask"
+
+    assert_text "I don't care, get dressed and go to work!"
+  end
+end
+  # test "visiting the index" do
+  #   visit questions_url
+  #
+  #   assert_selector "h1", text: "Questions"
+  # end
+end
